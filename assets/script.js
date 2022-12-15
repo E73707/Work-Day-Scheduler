9aa35ed749e72a15c3de8b35888bf561f2146379
E73707 <94885681+E73707@users.noreply.github.com>
2022-12-15 19:35:55 +1100

    finished

--- assets/script.js
+++ assets/script.js
@@ -1,90 +1,83 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
-saveBtn = $(".saveBtn");
-
-function getlocal(item) {
-  JSON.parse(window.localStorage.getItem(`${item}`));
-}
-
-function setlocal(item) {
-  window.localStorage.setItem(`${item}`, JSON.stringify(item));
-}
-// submits item and sores it to local system with ID
-let submitItem = function () {
-  event.preventDefault();
-  var btn = $(event.target);
-  // console.log(btn);
-  var textValue = btn.siblings("textarea").val().trim();
-  var btnId = btn.parent().attr("id");
-  window.localStorage.setItem(`${btnId}`, JSON.stringify(textValue));
-};
-
-// interval for keeping track of time
-setInterval(function () {
-  time = dayjs().format("h:mm:ss");
-  $("#currentTime").text(time);
-});
-
-var time24hr = dayjs().hour();
-
-for (
-  let i = 0;
-  i < $("#container").children(".row").children("textarea").length;
-  i++
-) {
-  let divi = $("#container").children(".row")[i];
-  $("#container").children(".row")[i];
-  newItem = JSON.parse(window.localStorage.getItem(divi.id));
-  if (newItem === null) {
-    $("textarea")[i].append("");
-  } else {
-    $("textarea")[i].append(newItem);
-  }
-}
-
-var date = dayjs().format("MMMM D, YYYY");
-$("#currentDay").text(date);
-
-for (
-  let i = 0;
-  i < $("#container").children(".row").children("textarea").length;
-  i++
-) {
-  let divi = $("#container").children(".row")[i];
-  if (divi.id.slice(5) < time24hr) {
-    $(`#hour-${divi.id.slice(5)}`).css("background-color", "red");
-  } else if (divi.id.slice(5) == time24hr) {
-    $(`#hour-${divi.id.slice(5)}`).css("background-color", "grey");
-  } else {
-    $(`#hour-${divi.id.slice(5)}`).css("background-color", "green");
-  }
-
-  //   divi = divi
-}
-
-console.log(localStorage);
+// // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// // the code isn't run until the browser has finished rendering all the elements
+// // in the html.
 
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+  let saveBtn = $(".saveBtn");
 
-saveBtn.on("click", submitItem);
+  let submitItem = function () {
+    event.preventDefault();
+    var btn = saveBtn;
+    var textValue = btn.siblings("textarea").val().trim();
+    var btnId = btn.parent().attr("id");
+    window.localStorage.setItem(`${btnId}`, JSON.stringify(textValue));
+  };
+  saveBtn.on("click", submitItem);
+
+  setInterval(function () {
+    time = dayjs().format("h:mm:ss");
+    $("#currentTime").text(time);
+  });
+
+  var time24hr = dayjs().hour();
+  window.localStorage.setItem("time24hr", JSON.stringify(time24hr));
+
+  let refreshColors = function () {
+    for (
+      let i = 0;
+      i < $("#container").children(".row").children("textarea").length;
+      i++
+    ) {
+      let divi = $("#container").children(".row")[i];
+      if (divi.id.slice(5) < time24hr) {
+        $(`#hour-${divi.id.slice(5)}`).css("background-color", "red");
+      } else if (divi.id.slice(5) == time24hr) {
+        $(`#hour-${divi.id.slice(5)}`).css("background-color", "grey");
+      } else {
+        $(`#hour-${divi.id.slice(5)}`).css("background-color", "green");
+      }
+    }
+  };
+
+  refreshOnHour = function () {
+    console.log("start");
+    minsTillRefresh = 60 - dayjs().format("mm");
+    secsTillRefresh = 60 - dayjs().format("ss");
+    if (minsTillRefresh == 0) {
+      minsTillRefresh = 60;
+    }
+    if (secsTillRefresh == 0) {
+      secsTillRefresh = 60;
+    }
+    setInterval(function () {
+      time24hr = dayjs().hour();
+      window.localStorage.setItem(`time24hr`, JSON.stringify(time24hr));
+      console.log("done");
+      refreshColors();
+      refreshOnHour();
+      return time24hr;
+    }, 1000 * 60 * minsTillRefresh + secsTillRefresh * 1000);
+  };
+
+  refreshColors();
+  refreshOnHour();
+
+  for (
+    let i = 0;
+    i < $("#container").children(".row").children("textarea").length;
+    i++
+  ) {
+    let divi = $("#container").children(".row")[i];
+    $("#container").children(".row")[i];
+    newItem = JSON.parse(window.localStorage.getItem(divi.id));
+    if (newItem === null) {
+      $("textarea")[i].append("");
+    } else {
+      $("textarea")[i].append(newItem);
+    }
+  }
+
+  var date = dayjs().format("MMMM D, YYYY");
+  $("#currentDay").text(date);
+  saveBtn.on("click", submitItem);
+});
